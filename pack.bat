b4c284ee2f2c64211dfeefe435b350a9d262f118
Dmitriy <dima3014@gmail.com>
2025-11-19 11:18:17 +0200

    Changes for first copy

--- pack.bat
+++ pack.bat
@@ -1,10 +1,10 @@
 @echo off
 IF NOT EXIST 7z.exe GOTO NO7Z
-IF NOT EXIST "Twitch Drops Miner" mkdir "Twitch Drops Miner"
+IF NOT EXIST "Twitch Drops Miner First" mkdir "Twitch Drops Miner First"
 rem Prepare files
-copy /y /v dist\*.exe "Twitch Drops Miner"
-copy /y /v manual.txt "Twitch Drops Miner"
-IF EXIST "Twitch Drops Miner.zip" (
+copy /y /v dist\*.exe "Twitch Drops Miner First"
+copy /y /v manual.txt "Twitch Drops Miner First"
+IF EXIST "Twitch Drops Miner First.zip" (
     rem Add action
     set action=a
 ) ELSE (
@@ -12,10 +12,10 @@ IF EXIST "Twitch Drops Miner.zip" (
     set action=u
 )
 rem Pack and test
-7z %action% "Twitch Drops Miner.zip" "Twitch Drops Miner/" -r
-7z t "Twitch Drops Miner.zip" * -r
+7z %action% "Twitch Drops Miner First.zip" "Twitch Drops Miner First/" -r
+7z t "Twitch Drops Miner First.zip" * -r
 rem Cleanup
-IF EXIST "Twitch Drops Miner" rmdir /s /q "Twitch Drops Miner"
+IF EXIST "Twitch Drops Miner First" rmdir /s /q "Twitch Drops Miner First"
 GOTO EXIT
 :NO7Z
 echo No 7z.exe detected, skipping packaging!
